3139bdde9de7fb14415594fbd2b4fc12cde95291
Shreenabh Agrawal <me@shreenabh.com>
2022-01-30 18:13:13 +0530

    Create 1A.c

---
+++ 1A.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int size;
+    scanf("%d", &size);
+    int *array=malloc(sizeof(int)*size);
+    for(int i=0; i<size; i++){
+        scanf("%d", &array[i]);
+    }
+    //check if i+1 th element is larger than ith elemnt
+    int large_count=0;
+    for(int i=0; i<size-1; i++){
+        if(array[i]<array[i+1]){
+            large_count++;
+        }
+    }
+    printf("%d\n", large_count);
+
+}
